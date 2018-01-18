f0485e46ffec1f241841e078cb714826e523e3d3
Bassyouni <Bassyouni@192.168.211.128>
2018-01-18 18:15:49 +0200

    Stub

---
+++ Smart-Home_IoT-GP/Smart-Home_IoT-GP/Mqtt.swift
@@ -0,0 +1,14 @@
+//
+//  Mqtt.swift
+//  Smart-Home_IoT-GP
+//
+//  Created by Bassyouni on 1/18/18.
+//  Copyright © 2018 Bassyouni. All rights reserved.
+//
+
+import Foundation
+
+class Mqtt
+{
+    
+}
